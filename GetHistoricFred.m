9bb995447d06c958b9799470a32dc9533dd97d3e
Me <Me@DESKTOP-HR2SABB>
2017-07-30 10:01:13 -0300

    updating all eua variables

--- GetHistoricFred.m
+++ GetHistoricFred.m
@@ -1,6 +1,7 @@
 function [ data ] = GetHistoricFred(symbol,startDate,endDate)
-%   Forked by Julio Lima (Universidade Federal do CearÃ¡)
-%   Query date ranges from Fre finance.
+%   Produced by Chris Reeves (A2X Capital LLC)
+%   Forked by Julio Lima (Universidade Federal do Cear�)
+%   Query date ranges from Fred finance.
 %   Sample usage: GetHistoricFred('M1','04/27/2010','04/27/2017')
 %   Symbols to use: 
 %   |-> M1 Money Stock: M1
@@ -10,15 +11,16 @@ function [ data ] = GetHistoricFred(symbol,startDate,endDate)
 %   |-> Real Personal Consumption Expenditures: PCEC96
 
     %Define a format of query.
-    format = 'yyyy-mm-dd';
+    %formatIn = 'dd-mmm-yyyy' or 'mm/dd/yyyy' or 'mm-dd-yyyy'
+    formatUrl = 'yyyy-mm-dd';
 
     %First transform to the number format, it's a generic one.
     startDateNum = datenum(startDate);
     endDateNum = datenum(endDate);
 
     %First transform to the number format, it's a generic one.
-    startDateStr = datestr(startDateNum, format);
-    endDateStr = datestr(endDateNum, format);
+    startDateStr = datestr(startDateNum, formatUrl);
+    endDateStr = datestr(endDateNum, formatUrl);
 
     %Define how much attributes will be used.
     n_attb = 3;
@@ -37,13 +39,35 @@ function [ data ] = GetHistoricFred(symbol,startDate,endDate)
     
     url = char(root);
     
-    %%Display address URL.
-    disp(url)
+    %Display address URL.
+   % disp(url)
     
-    %Receive the file;
+    %Receive the file.
     response = urlread(url);
     
-    %Scan and convert the file to cells and return. peido
-    data = textscan(response,'%s %f','delimiter',',','HeaderLines',1);  
+    %Scan and convert the file to cells and return.
+    data_in = textscan(response,'%s %s','delimiter',',','HeaderLines',1);    
     
+    %Filter data out by order.
+    size_max = size(data_in{1,1});
+    n_row = size_max(1,1);
+    data = cell(n_row,2);
+    data(:,1) = data_in{1,1};
+    data(:,2) = data_in{1,2};
+    
+    %Change datetime format to numbers and override the unreadble parameters.
+    try
+        i = 1;
+        while i<=n_row
+            %Change.
+            data{i,1} = datenum(data{i,1});
+            %Override.
+            if(strcmp(data(i,2),'.') || strcmp(data(i,2),'-'))
+                data(i,:) = [];
+                i = i - 1;
+            end
+            i = i + 1;
+        end
+    catch       
+    end
 end
--- GetHistoricGoogle.m
+++ GetHistoricGoogle.m
@@ -1,16 +1,59 @@
 function [ data ] = GetHistoricGoogle( symbol,startDate,endDate )
 %   Produced by Chris Reeves (A2X Capital LLC)
-%   query date ranges from google finance
-%   sample usage GetHistoricGoogle('AAPL','04/27/2010','04/27/2017')
+%   Forked by Julio Lima (Universidade Federal do Ceará)
+%   Query date ranges from Google finance.
+%   Sample usage GetHistoricGoogle('AAPL','04/27/2010','04/27/2017')
+%   Order: 1-Date, 2-Open, 3-High, 4-Low, 5-Close, 6-Volume
+
+    % Checking for optional Variables
+    if ~exist('Order', 'var')
+        order = 2;
+    end
+
+    %Define a format of query.
+    %formatIn = 'dd-mmm-yyyy' or 'mm/dd/yyyy' or 'mm-dd-yyyy'
+    formatUrl = 'yyyy-mm-dd';
+
+    
+    %First transform to the number format, it's a generic one.
     startDateNum = datenum(startDate);
     endDateNum = datenum(endDate);
 
-    startDateStr = datestr(startDateNum,'mmm dd, yyyy');
-    endDateStr = datestr(endDateNum,'mmm dd, yyyy');
+    %First transform to the number format, it's a generic one.
+    startDateStr = datestr(startDateNum,formatUrl);
+    endDateStr = datestr(endDateNum,formatUrl);
 
     url = strcat('http://www.google.com/finance/historical?q=',symbol,'&startdate=',startDateStr,'&enddate=',endDateStr,'&output=csv');
     url = strrep(url,' ','%20');
+    
+    %Display address URL.
+    %disp(url)
+    
+    %Receive the file and fix.
     response = urlread(url);
-    data = textscan(response,'%s %f %f %f %f %f','delimiter',',','HeaderLines',1);
-
+    data_in = textscan(response,'%s %s %s %s %s %s','delimiter',',','HeaderLines',1);
+    
+    %Filter data out by order.
+    n_row = size(data_in{1,1});
+    n_row = n_row(1,1);
+    data = cell(n_row(1,1),2);
+    data(:,1) = data_in{1,1};
+    data(:,2) = data_in{1,order};
+    
+    %Change datetime format to numbers and override the unreadble parameters.
+    try
+        i = 1;
+        while i<n_row
+            %Change.
+            data{i,1} = datenum(data{i,1});
+            %Override.
+            if(strcmp(data(i,2),'.') || strcmp(data(i,2),'-'))
+                data(i,:) = [];
+                i = i - 1;
+            end
+            i = i + 1;
+        end
+    catch       
+    end
+    
 end
